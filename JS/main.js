7781ef2703ae2b88836435ec19517bb3d2dd3b5d
Ivan Dziuba <ivaan.id@gmail.com>
2021-08-26 20:35:58 -0300

    listo

---
+++ JS/main.js
@@ -0,0 +1,5 @@
+var saludo = "¡Hola xeneize!"
+var entradaNombre = prompt ("Ingresa tu nombre");
+var entradaApellido = prompt ("Ingresa tu apellido");
+var salida = saludo + " " + entradaNombre + " " + entradaApellido;
+alert (salida);
